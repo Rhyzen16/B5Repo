011d9f9d691e2af47ede6f64db07e414058584b5
Rhyzen16 <102942367+Rhyzen16@users.noreply.github.com>
2023-04-09 18:57:30 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,70 @@
+function validateForm() {
+
+  const username = document.getElementById("username").value;
+
+  const password = document.getElementById("password").value;
+
+  if (username === "" || password === "") {
+
+    document.getElementById("error-message").innerHTML = "Please fill out both fields.";
+
+    return false;
+
+  }
+
+  return true;
+
+}
+
+function loginUser() {
+
+  const username = document.getElementById("username").value;
+
+  const password = document.getElementById("password").value;
+
+  // Here, you would typically make an API call to verify the credentials
+
+  // In this example, we're just checking if the username and password match
+
+  if (username === "admin" && password === "password") {
+
+    alert("Login successful!");
+
+    window.location.href = "https://manhuafast.net/"; 
+
+    return true;
+
+  } else {
+
+    alert("Invalid username or password.");
+
+  }
+
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    if (validateForm()) {
+
+      loginUser();
+
+    }
+
+  });
+
+});
+
+function showMessage() {
+
+  console.log("This message will be displayed after 5 seconds");
+
+}
+
+setTimeout(showMessage, 5000);
+
